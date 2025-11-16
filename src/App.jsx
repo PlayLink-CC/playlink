85f40eed89fb983b9c97dfafc80f3f6b1309d574
Saviskar <Saviskar>
2025-11-16 17:50:49 +0530

    refactor: implement MainLayout with navbar, footer, and outlet structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import SignUp from "./pages/Signup";
+import MainLayout from "./components/layout/MainLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public pages */}
-        <Route path="/" element={<Home />} />
-
         {/* Auth pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+
+        {/* All routes inside this Route will use MainLayout */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
